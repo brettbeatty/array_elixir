f8cffaa5f4e2a009f0a8222272d767184da8b307
Brett Beatty <brettbeatty@gmail.com>
2020-07-25 23:08:30 -0600

    Implement Array.new/0

--- lib/array.ex
+++ lib/array.ex
@@ -4,4 +4,26 @@ defmodule Array do
 
   This module is meant as a learning exercise, not an optimized data structure.
   """
+  @type t() :: t(any())
+  @type t(_element) :: %__MODULE__{
+          elements: tuple(),
+          size: non_neg_integer(),
+          start: non_neg_integer()
+        }
+
+  defstruct elements: :erlang.make_tuple(8, nil), size: 0, start: 0
+
+  @doc """
+  Creates an empty array.
+
+  ## Examples
+
+      iex> Array.new()
+      #Array<[]>
+
+  """
+  @spec new() :: t()
+  def new do
+    %__MODULE__{}
+  end
 end
--- test/array_test.exs
+++ test/array_test.exs
@@ -1,3 +1,9 @@
 defmodule ArrayTest do
   use ExUnit.Case, async: true
+
+  describe "Array.new/0" do
+    test "creates an empty array" do
+      assert Array.new() == %Array{}
+    end
+  end
 end
