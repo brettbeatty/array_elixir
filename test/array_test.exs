280f07b6425c99c5073dd7e835b7f4bed6c0487a
Brett Beatty <brettbeatty@gmail.com>
2020-07-26 01:13:16 -0600

    Renovate tests to use new/1 and to_list/1

--- test/array_test.exs
+++ test/array_test.exs
@@ -3,34 +3,37 @@ defmodule ArrayTest do
 
   describe "Array.new/0" do
     test "creates an empty array" do
-      assert Array.new() == %Array{}
+      array = Array.new()
+
+      assert Array.to_list(array) == []
     end
   end
 
   describe "Array.new/1" do
     test "creates an array from an enumerable" do
-      expected = %Array{elements: {:a, :b, :c, nil, nil, nil, nil, nil}, size: 3, start: 0}
+      array = Array.new([:a, :b, :c])
 
-      assert Array.new([:a, :b, :c]) == expected
+      assert Array.to_list(array) == [:a, :b, :c]
     end
   end
 
   describe "Array.push/2" do
     test "appends an element to an array" do
-      array = %Array{elements: {nil, nil, :a, :b}, size: 2, start: 2}
-      expected = %Array{elements: {:c, nil, :a, :b}, size: 3, start: 2}
+      array =
+        [:a, :b]
+        |> Array.new()
+        |> Array.push(:c)
 
-      assert Array.push(array, :c) == expected
+      assert Array.to_list(array) == [:a, :b, :c]
     end
   end
 
   describe "Array.shift/1" do
     test "removes the first element from an array" do
       array = Array.new([:a, :b, :c])
-      expected = %{array | start: 1, size: 2}
 
       assert {:a, new_array} = Array.shift(array)
-      assert new_array == expected
+      assert Array.to_list(new_array) == [:b, :c]
     end
 
     test "returns :error if array is empty" do
@@ -50,10 +53,10 @@ defmodule ArrayTest do
 
   describe "Collectable" do
     test "collects elements into an array" do
-      array = %Array{elements: {nil, nil, nil, :a}, size: 1, start: 3}
-      expected = %Array{elements: {:b, :c, nil, :a}, size: 3, start: 3}
+      array = Array.new([:a])
+      new_array = Enum.into([:b, :c], array)
 
-      assert Enum.into([:b, :c], array) == expected
+      assert Array.to_list(new_array) == [:a, :b, :c]
     end
   end
 
