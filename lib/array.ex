8b6649b749a645130c0ec654418ef65529ad8022
Brett Beatty <brettbeatty@gmail.com>
2020-08-30 01:36:25 -0600

    Implement Array.shift/1

--- lib/array.ex
+++ lib/array.ex
@@ -19,4 +19,48 @@ defmodule Array do
   @type t() :: t(any())
 
   defstruct ~W[elements size start]a
+
+  @spec element_position(t(), integer()) :: non_neg_integer()
+  defp element_position(array, index) do
+    capacity = tuple_size(array.elements)
+
+    case rem(array.start + index, capacity) do
+      remainder when remainder >= 0 ->
+        remainder
+
+      remainder ->
+        remainder + capacity
+    end
+  end
+
+  @doc """
+  Shifts the first element from an array, returning it and the updated array.
+
+  Returns :error if array is empty.
+
+  ## Examples
+
+      iex> array = Array.new([:z, :a, :b, :c])
+      iex> {:ok, :z, new_array} = Array.shift(array)
+      iex> new_array
+      #Array<[:a, :b, :c]>
+
+      iex> array = Array.new()
+      iex> Array.shift(array)
+      :error
+
+  """
+  @spec shift(array :: t(element)) :: {:ok, element, t(element)} | :error when element: var
+  def shift(array)
+
+  def shift(%{size: 0}) do
+    :error
+  end
+
+  def shift(array) do
+    element = elem(array.elements, array.start)
+    new_array = %{array | size: array.size - 1, start: element_position(array, 1)}
+
+    {:ok, element, new_array}
+  end
 end
--- test/array_test.exs
+++ test/array_test.exs
@@ -1,3 +1,18 @@
 defmodule ArrayTest do
   use ExUnit.Case, async: true
+
+  describe "shift/1" do
+    test "shifts the first element off an array" do
+      array = %Array{elements: {:b, :a}, size: 2, start: 1}
+      expected_array = %Array{elements: {:b, :a}, size: 1, start: 0}
+
+      assert {:ok, :a, ^expected_array} = Array.shift(array)
+    end
+
+    test "returns :error when shifting off an empty array" do
+      array = %Array{elements: {nil, nil}, size: 0, start: 0}
+
+      assert Array.shift(array) == :error
+    end
+  end
 end
