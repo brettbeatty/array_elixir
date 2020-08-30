2c63a394c50697d0db40acd3d62f08351da2d4bf
Brett Beatty <brettbeatty@gmail.com>
2020-08-29 20:33:50 -0600

    Generate new project

---
+++ lib/array.ex
@@ -0,0 +1,18 @@
+defmodule Array do
+  @moduledoc """
+  Documentation for `Array`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Array.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
